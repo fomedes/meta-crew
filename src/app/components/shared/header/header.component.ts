fd9e7aed6d6d306f738dcb1a241c9b6fb429a60a
[Francisco Omedes] <[fomedes@uoc.edu]>
2024-01-22 13:34:53 +1100

    added teams in header + condition bar format

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { SharedService } from 'src/app/services/shared.service';
 
 @Component({
@@ -6,8 +7,17 @@ import { SharedService } from 'src/app/services/shared.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private sharedService: SharedService) {}
+export class HeaderComponent implements OnInit {
+  teams!: any[];
+
+  constructor(private sharedService: SharedService, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('assets/teams/teams_id.json').subscribe((data) => {
+      this.teams = data.sort((a, b) => a.name.localeCompare(b.name));
+    });
+  }
+
   navigationTo(route: string): void {
     this.sharedService.navigationTo(route);
   }
