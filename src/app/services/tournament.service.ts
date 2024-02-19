d61936795dcf470ddc81eb57c8101742a2501674
[Francisco Omedes] <omedesf@gmail.com>
2024-02-20 01:20:10 +1100

    S3 updated groups

--- src/app/services/tournament.service.ts
+++ src/app/services/tournament.service.ts
@@ -15,6 +15,6 @@ export class TournamentsService {
 
   getGroupData(teamId: any): Observable<any> {
     const apiUrl = `https://api.metasoccer.com/msl/group/${teamId.groupId}`;
-    return this.http.get(apiUrl).pipe(timeout(30000));
+    return this.http.get(apiUrl).pipe(timeout(40000));
   }
 }
