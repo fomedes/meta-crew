5bde841b58ef8f0fa399bcc71b0de0ef985c9c34
[Francisco Omedes] <omedesf@gmail.com>
2025-04-25 00:45:58 +0100

    Fixed environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,11 @@
-
-
 export const environment = {
-  production: true,
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  production: false,
+  SUPABASE_URL: 'https://ykxiamrtzkpxccpcbgjp.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlreGlhbXJ0emtweGNjcGNiZ2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDAyNTk0NywiZXhwIjoyMDU5NjAxOTQ3fQ.O5rmYYFoxBbGXXi-uEg2Gsg7SQUXc1yLA9BlRYre9IA'
 };
+
+// export const environment = {
+//   production: true,
+//   SUPABASE_URL: '',
+//   SUPABASE_ANON_KEY: '',
+// };
